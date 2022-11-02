5263ef6258d9494da33e1615d0a9dd2de8411352
Angel Ramos <angelrc1998@hotmail.com>
2022-11-01 19:23:16 -0500

    Initial Commit

---
+++ Venegas Tax Service/Venegas_Tax_ServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Venegas_Tax_ServiceApp.swift
+//  Venegas Tax Service
+//
+//  Created by Vigitrack on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Venegas_Tax_ServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
